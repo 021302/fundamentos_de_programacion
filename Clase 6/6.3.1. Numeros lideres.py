e40e52914d337389163750d1d1193b7c499059bd
Emma Daniela Palacios Rodriguez <daniela.palaciosrodriguez1@gmial.com>
2025-06-02 18:36:30 -0600

    Tarea 9

--- Clase 6/6.3.1. Numeros lideres.py
+++ Clase 6/6.3.1. Numeros lideres.py
@@ -1,3 +1,14 @@
+'''
+Clase:        Clase 6
+Tema:         Listas
+Ejercicio:    Números Líderes
+Descripción:  Crear un programa que detecte si un número es mayor que el que le sigue, y así sucesivamente. Debe de imprimir dichos números
+
+Autor:        Emma Daniela Palacios Rodriguez
+Fecha:        2025-06-02
+Estado:       [ Terminado ]
+'''
+
 a = list(map(int, input("Ingrese una serie de numeros: ").split(" ")))
 
 c = []  #lista vacia donde se iran agragando los numeros lideres
